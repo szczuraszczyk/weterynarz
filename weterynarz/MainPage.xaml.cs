e11c489b6141eb6f7dbfb4054339afa051c961bc
szcz <ameli@s>
2026-02-26 12:34:15 +0100

    c#

--- weterynarz/MainPage.xaml.cs
+++ weterynarz/MainPage.xaml.cs
@@ -8,5 +8,16 @@
             InitializeComponent();
         }
 
-    }
+        private void OnSlideW(object? sender, EventArgs e)
+        {
+            if (slider_wiek == null)
+            {
+                return;
+            }
+            double sliderD = slider_wiek.Value;
+            int slider = (int)Math.Round(sliderD);
+        ile_lat.Text = Convert.ToString(slider);
+        }
+
+}
 }
